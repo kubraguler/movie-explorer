006730dd38aa2038ca64cdefe66c2ad7ba9c230e
kubraguler <oktnkbr@gmail.com>
2024-01-13 23:07:32 +0000

    Add Loading component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,15 @@
+import "./Loading.css";
+
+const Loading = () => {
+	return (
+		<div className="flex items-center justify-center h-screen mt-16">
+			<div className="flex space-x-2">
+				<div className="w-4 h-4 bg-emerald-400 rounded-full animate-custom-bounce" style={{ animationDelay: "0.1s" }}></div>
+				<div className="w-4 h-4 bg-emerald-400 rounded-full animate-custom-bounce" style={{ animationDelay: "0.2s" }}></div>
+				<div className="w-4 h-4 bg-emerald-400 rounded-full animate-custom-bounce" style={{ animationDelay: "0.3s" }}></div>
+			</div>
+		</div>
+	);
+};
+
+export default Loading;

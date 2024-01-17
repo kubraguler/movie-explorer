b037a4a6a7266da5734bfe29433f1acd1a14a86e
kubraguler <oktnkbr@gmail.com>
2024-01-17 21:35:04 +0000

    Implement debounced search with lodash

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,18 +1,35 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import _ from "lodash";
+import { setQuery, searchMovies } from "../../redux/reducers/moviesSlice";
 
 const SearchBar = () => {
-	const [query, setQuery] = useState("");
+	const dispatch = useDispatch();
+	const { query, page } = useSelector((state) => state.movies);
+
+	const handleSearch = () => {
+		dispatch(searchMovies({ query, page }));
+	};
+
+	const debouncedHandleSearch = _.debounce(handleSearch, 500);
+
+	useEffect(() => {
+		debouncedHandleSearch();
+		return () => debouncedHandleSearch.cancel();
+	}, [query]);
 
 	return (
-		<div className="col-start-2 col-span-2 max-w-xl">
-			<input
-				className="bg-zinc-300 py-4 px-20 w-full shrink-0 rounded-full text-xl font-normal text-center text-black placeholder:text-black"
-				type="text"
-				placeholder="🔍 Search a movie or a series"
-				value={query}
-				onChange={(e) => setQuery(e.target.value)}
-			/>
-		</div>
+		<>
+			<div className="col-start-2 col-span-2 max-w-xl">
+				<input
+					className="bg-zinc-300 py-4 px-20 w-full shrink-0 rounded-full text-xl font-normal text-center text-black placeholder:text-black"
+					type="text"
+					placeholder="🔍 Search a movie or a series"
+					value={query}
+					onChange={(e) => dispatch(setQuery(e.target.value))}
+				/>
+			</div>
+		</>
 	);
 };
 
--- src/redux/reducers/moviesSlice.js
+++ src/redux/reducers/moviesSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const baseURL = "https://api.themoviedb.org/3/movie/popular";
+const baseURL = "https://api.themoviedb.org/3";
 
 const options = {
 	method: "GET",
@@ -10,22 +10,35 @@ const options = {
 	}
 };
 
-export const getMovies = createAsyncThunk("movies/getMovies", async (page = 1) => {
-	const response = await fetch(`${baseURL}?page=${page}`, options);
+const initialState = {
+	movies: [],
+	loading: false,
+	error: null,
+	page: 1,
+	totalPage: 1,
+	query: ""
+};
+
+export const getMovies = createAsyncThunk("getPopularMovies", async (page = 1) => {
+	const response = await fetch(`${baseURL}/movie/popular?page=${page}`, options);
+	const data = await response.json();
+	return data;
+});
+
+export const searchMovies = createAsyncThunk("searchMovies", async ({ query, page = 1 }) => {
+	const response = await fetch(`${baseURL}/search/movie?query=${query}&page=${page}`, options);
 	const data = await response.json();
 	return data;
 });
 
 const moviesSlice = createSlice({
 	name: "movies",
-	initialState: {
-		movies: [],
-		loading: false,
-		error: null,
-		page: 1,
-		totalPage: 1
+	initialState,
+	reducers: {
+		setQuery: (state, action) => {
+			state.query = action.payload;
+		}
 	},
-	reducers: {},
 	extraReducers: (builder) => {
 		builder.addCase(getMovies.pending, (state) => {
 			state.loading = true;
@@ -40,7 +53,21 @@ const moviesSlice = createSlice({
 			state.loading = false;
 			state.error = error.message;
 		});
+		builder.addCase(searchMovies.pending, (state) => {
+			state.loading = true;
+		});
+		builder.addCase(searchMovies.fulfilled, (state, action) => {
+			state.loading = false;
+			state.movies = [...state.movies, ...action.payload.results];
+			state.page = action.payload.page;
+			state.totalPage = action.payload.total_pages;
+		});
+		builder.addCase(searchMovies.rejected, (state, error) => {
+			state.loading = false;
+			state.error = error.message;
+		});
 	}
 });
 
+export const { setQuery } = moviesSlice.actions;
 export default moviesSlice.reducer;
